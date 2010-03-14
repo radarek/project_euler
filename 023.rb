00b475b2e2f279f8b30bee9a000649336c2ed106
Radosław Bułat <radek.bulat@gmail.com>
2010-03-14 21:03:54 +0100

    Ruby solution for 23rd problem.

---
+++ 023.rb
@@ -0,0 +1,26 @@
+# http://projecteuler.net/index.php?section=problems&id=23
+# 
+# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+# 
+# A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+# 
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+# 
+# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+def proper_divisors(n)
+  return 1.upto(n / 2).find_all {|e| n % e == 0 }
+end
+
+$abundant = Hash.new {|h, k|
+  h[k] = proper_divisors(k).inject(:+) > k
+}
+def abundant?(n)
+  return $abundant[n]
+end
+
+def abundant_summable?(n)
+  return 2.upto(n - 2).any? {|e| abundant?(e) && abundant?(n - e) }
+end
+
+puts 1.upto(28123).find_all {|e| !abundant_summable?(e) }.inject(:+)
